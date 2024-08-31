b6dbfa1ef0d5e3c26f85acfc72baf5d864b35417
Şenol Ataç <senolatac@Senols-MBP.home>
2024-08-31 21:47:55 +0300

    initial

--- README.md
+++ README.md
@@ -27,6 +27,31 @@ curl -X 'POST' \
   "input_args_as_key_value": {"x": 3, "y": 5}
 }'
 ```
+- Json Example
+```
+curl -X 'POST' \
+  'http://localhost:9595/python-scripts/exec' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Basic dXNlcjpwYXNz' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "command_text": "x =  '\''{ \"name\":\"John\", \"age\":30, \"city\":\"New York\"}'\''\ny = json.loads(x)\nage=y[\"age\"]",
+  "input_args_as_key_value": {"x": 5, "y": 4},
+  "output_args_keys": ["age"]
+}'
+```
+- Request Example
+```
+curl -X 'POST' \
+  'http://localhost:9595/python-scripts/eval' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Basic dXNlcjpwYXNz' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "command_text": "requests.get('\''https://w3schools.com/python/demopage.htm'\'').text",
+  "input_args_as_key_value": {}
+}'
+```
 
 #### Build and publish
 ##### Build
--- app/service/exec_service.py
+++ app/service/exec_service.py
@@ -2,17 +2,25 @@ from app.schema.exec_command import ExecCommand
 from app.schema.eval_command import EvalCommand
 
 import math
+import requests
+import random
+import json
 
 ALLOWED_NAMES = {
-    k: v for k, v in math.__dict__.items() if not k.startswith("__")
+    k: v for d in [math.__dict__, requests.__dict__, json.__dict__] for k,v in d.items() if not k.startswith("__")
 }
 ALLOWED_NAMES['math'] = math
+ALLOWED_NAMES['requests'] = requests
+ALLOWED_NAMES['random'] = random
+ALLOWED_NAMES['json'] = json
 
 def execute(exec_command_body: ExecCommand):
-    safe_dict = _safe_commands()
-    exec_command_body.input_args_as_key_value["__builtins__"] = None
+    input_args = exec_command_body.input_args_as_key_value or {}
+    #_validate_input("exec", exec_command_body.command_text, input_args)
+    safe_dict = ALLOWED_NAMES
+    input_args["__builtins__"] = None
 
-    exec(exec_command_body.command_text, exec_command_body.input_args_as_key_value, safe_dict)
+    exec(exec_command_body.command_text, input_args, safe_dict)
 
     outputs = {}
     for output_arg in exec_command_body.output_args_keys:
@@ -22,19 +30,27 @@ def execute(exec_command_body: ExecCommand):
 
 
 def evaluate(eval_command_body: EvalCommand):
+    input_args = eval_command_body.input_args_as_key_value or {}
+    #_validate_input("eval", eval_command_body.command_text, input_args)
+
+    input_args["__builtins__"] = None
+
+    return eval(eval_command_body.command_text, input_args, ALLOWED_NAMES)
+
+
+def _validate_input(type, text, input_args):
     """Evaluate a math expression."""
     # Compile the expression
-    code = compile(eval_command_body.command_text, "<string>", "eval")
+    _validate_allowed_commands(type, text, input_args)
+    for value in input_args.values():
+        _validate_allowed_commands(type, value, {})
 
-    # Validate allowed names
-    #for name in code.co_names:
-    #    if name not in ALLOWED_NAMES:
-    #        raise NameError(f"The use of '{name}' is not allowed")
 
-    safe_dict = ALLOWED_NAMES
-    eval_command_body.input_args_as_key_value["__builtins__"] = None
-
-    return eval(eval_command_body.command_text, eval_command_body.input_args_as_key_value, safe_dict)
+def _validate_allowed_commands(type, text, input_args):
+    code = compile(str(text), "<string>", type)
+    for name in code.co_names:
+        if name not in ALLOWED_NAMES and name not in input_args:
+            raise NameError(f"The use of '{name}' is not allowed")
 
 
 def _safe_commands():
